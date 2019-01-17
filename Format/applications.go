9bb34ad7b7f6f5d5deb0af8263d2a40a2488a868
Ed <edward.dobson3@ntlworld.com>
2019-01-17 13:30:06 +0000

    Show number of ready components when listing applications

--- Format/applications.go
+++ Format/applications.go
@@ -5,17 +5,24 @@ import (
 	"github.com/GodlikePenguin/agogos-cli/Errors"
 	"github.com/GodlikePenguin/agogos-datatypes"
 	"os"
+	"strings"
 	"text/tabwriter"
 )
 
 func PrintApplications(apps *[]Datatypes.Application) error {
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
-	_, err := fmt.Fprintln(w, "NAME\t#COMPONENTS\t#COPIES\tMESSAGES")
+	_, err := fmt.Fprintln(w, "NAME\tCOMPONENTS READY\t#COPIES\tMESSAGES")
 	if err != nil {
 		return Errors.FormatError()
 	}
 	for _, app := range *apps {
-		_, err := fmt.Fprintln(w, fmt.Sprintf("%s\t%d\t%d\t%s", app.Name, len(app.Components), app.Copies, app.Messages))
+		readyComps := 0
+		for _, comp := range app.Components {
+			if strings.Contains(comp.Status, "running") {
+				readyComps += 1
+			}
+		}
+		_, err := fmt.Fprintln(w, fmt.Sprintf("%s\t(%d/%d)\t%d\t%s", app.Name, readyComps, len(app.Components), app.Copies, app.Messages))
 		if err != nil {
 			return err
 		}
