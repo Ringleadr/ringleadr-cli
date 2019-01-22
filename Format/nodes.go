b1f5d426ec80816d18e70cc5308c16163601c7fa
Ed <edward.dobson3@ntlworld.com>
2019-01-22 14:38:47 +0000

    Add the concept of nodes being active and inactive

--- Format/nodes.go
+++ Format/nodes.go
@@ -10,12 +10,12 @@ import (
 
 func PrintNodes(nodes *[]Datatypes.Node) error {
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
-	_, err := fmt.Fprintln(w, "NAME\tADDRESS")
+	_, err := fmt.Fprintln(w, "NAME\tADDRESS\tACTIVE")
 	if err != nil {
 		return Errors.FormatError()
 	}
 	for _, node := range *nodes {
-		_, err := fmt.Fprintln(w, fmt.Sprintf("%s\t%s", node.Name, node.Address))
+		_, err := fmt.Fprintln(w, fmt.Sprintf("%s\t%s\t%t", node.Name, node.Address, node.Active))
 		if err != nil {
 			return err
 		}
